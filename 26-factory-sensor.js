5a3cdc0876ce398037732c92dfbeda6de6acdded
gab <114111654+gabalbcos@users.noreply.github.com>
2024-02-08 20:07:41 -0300

    add partial solution to 26

---
+++ 26-factory-sensor.js
@@ -0,0 +1,17 @@
+export function checkHumidityLevel(humidity) {
+  if (humidity > 70) {
+    throw new Error ('Oh, no... Humidity too high, no good!')
+  }
+}
+
+//instances of error
+//Broken Sensor
+class ArgumentError extends Erro{}
+
+function reportOverheating(temperature) {
+try {
+  
+} catch(error) {
+  
+}
+}
