991ef64d7a63bd508df10679b860b19b600a94de
gab <114111654+gabalbcos@users.noreply.github.com>
2024-02-13 22:17:22 -0300

    add solutionj to 26

--- 26-factory-sensor.js
+++ 26-factory-sensor.js
@@ -1,17 +1,52 @@
-export function checkHumidityLevel(humidity) {
-  if (humidity > 70) {
-    throw new Error ('Oh, no... Humidity too high, no good!')
-  }
-}
+ export function checkHumidityLevel(humidity) {
+    if (humidity > 70) {
+      throw new Error ('Oh, no... Humidity too high, no good!')
+    }
+ }
 
 //instances of error
 //Broken Sensor
-class ArgumentError extends Erro{}
+export class ArgumentError extends Error{
+  constructor() {
+    super('Argument Error');
+  }
+}
 
-function reportOverheating(temperature) {
-try {
-  
-} catch(error) {
-  
+export class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature ${temperature} is too high! Overheating!`);
+    this.temperature = temperature;
+  }
 }
+
+
+export function reportOverheating(temperature) {
+   if(temperature === null) {
+     throw new ArgumentError;
+ } else if (temperature > 500) {
+     throw new OverheatingError(temperature); 
+ } else {
+     return
+ }
 }
+
+export function monitorTheMachine(actions) {
+    try {
+      actions.check();
+    } catch(error) {
+      switch(error.constructor) {
+        case ArgumentError:
+          actions.alertDeadSensor();
+          return;
+        case OverheatingError:
+          if(error.temperature > 600) {
+            actions.shutdown();            
+            return;
+          } if (error.temperature > 500) {
+            actions.alertOverheating();
+            return;
+          }
+      } 
+        throw error;
+    }
+  }
