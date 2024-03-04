01cb711726df2d280580e9440b887c6fbf6fcbe2
gab <114111654+gabalbcos@users.noreply.github.com>
2024-03-03 21:02:13 -0300

    add eventListener and function to playButton

--- freecodecamp/music-player/script.js
+++ freecodecamp/music-player/script.js
@@ -101,12 +101,14 @@ const playSong = (id) => {
   audio.play();
 };
 
+
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -123,7 +125,13 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
