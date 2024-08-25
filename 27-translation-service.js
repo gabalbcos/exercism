9abdb5d178de99fd3c8aad4bb333a30aa202a50b
gab <114111654+gabalbcos@users.noreply.github.com>
2024-08-25 18:51:17 -0300

    add solution to exercise 27

--- 27-translation-service.js
+++ 27-translation-service.js
@@ -1,106 +1,62 @@
-/// <reference path="./global.d.ts" />
 // @ts-check
-//
-// The lines above enable type checking for this file. Various IDEs interpret
-// the @ts-check and reference directives. Together, they give you helpful
-// autocompletion when implementing this exercise. You don't need to understand
-// them in order to use it.
-//
-// In your own projects, files, and code, you can play with @ts-check as well.
-
-export class TranslationService {
-  /**
-   * Creates a new service
-   * @param {ExternalApi} api the original api
-   */
-  constructor(api) {
-    this.api = api;
-  }
-
-  /**
-   * Attempts to retrieve the translation for the given text.
-   *
-   * - Returns whichever translation can be retrieved, regardless the quality
-   * - Forwards any error from the translation api
-   *
-   * @param {string} text
-   * @returns {Promise<string>}
-   */
-  free(text) {
-    return this.api.fetch(text).then((result) => result.translation);
-  }
-
-  /**
-   * Batch translates the given texts using the free service.
-   *
-   * - Resolves all the translations (in the same order), if they all succeed
-   * - Rejects with the first error that is encountered
-   * - Rejects with a BatchIsEmpty error if no texts are given
-   *
-   * @param {string[]} texts
-   * @returns {Promise<string[]>}
-   */
-  batch(texts) {
-    throw new Error('Implement the batch function');
-  }
-
-  /**
-   * Requests the service for some text to be translated.
-   *
-   * Note: the request service is flaky, and it may take up to three times for
-   *       it to accept the request.
-   *
-   * @param {string} text
-   * @returns {Promise<void>}
-   */
-  request(text) {
-    throw new Error('Implement the request function');
-  }
-
-  /**
-   * Retrieves the translation for the given text
-   *
-   * - Rejects with an error if the quality can not be met
-   * - Requests a translation if the translation is not available, then retries
-   *
-   * @param {string} text
-   * @param {number} minimumQuality
-   * @returns {Promise<string>}
-   */
-  premium(text, minimumQuality) {
-    throw new Error('Implement the premium function');
-  }
-}
 
 /**
- * This error is used to indicate a translation was found, but its quality does
- * not meet a certain threshold. Do not change the name of this error.
+ * Given a certain command, help the chatbot recognize whether the command is valid or not.
+ *
+ * @param {string} command
+ * @returns {boolean} whether or not is the command valid
  */
-export class QualityThresholdNotMet extends Error {
-  /**
-   * @param {string} text
-   */
-  constructor(text) {
-    super(
-      `
-The translation of ${text} does not meet the requested quality threshold.
-    `.trim(),
-    );
 
-    this.text = text;
+export function isValidCommand(command) {
+  const result = /^chatbot/i.test(command);
+  return result;
+ }
+ 
+ /**
+  * Given a certain message, help the chatbot get rid of all the emoji's encryption through the message.
+  *
+  * @param {string} message
+  * @returns {string} The message without the emojis encryption
+  */
+ export function removeEmoji(message) {
+   const result = message.replace(/emoji\w*/g, '');
+   return result;
+ }
+ 
+ /**
+  * Given a certain phone number, help the chatbot recognize whether it is in the correct format.
+  *
+  * @param {string} number
+  * @returns {string} the Chatbot response to the phone Validation
+  */
+ export function checkPhoneNumber(number) {
+  const result = /^\(\+\d{2}\) \d{3}-\d{3}-\d{3}$/.test(number);
+  if(result) {
+    return "Thanks! You can now download me to your phone."
   }
-}
-
-/**
- * This error is used to indicate the batch service was called without any
- * texts to translate (it was empty). Do not change the name of this error.
- */
-export class BatchIsEmpty extends Error {
-  constructor() {
-    super(
-      `
-Requested a batch translation, but there are no texts in the batch.
-    `.trim(),
-    );
-  }
-}
+   return `Oops, it seems like I can't reach out to ${number}`;
+ }
+ 
+ /**
+  * Given a certain response from the user, help the chatbot get only the URL.
+  *
+  * @param {string} userInput
+  * @returns {string[] | null} all the possible URL's that the user may have answered
+  */
+ export function getURL(userInput) {
+   const regex = /\b[^\s/$.?#].[^\s]*\.[a-z]*\b/g;
+   return userInput.match(regex);
+ }
+ 
+ /**
+  * Greet the user using the full name data from the profile.
+  *
+  * @param {string} fullName
+  * @returns {string} Greeting from the chatbot
+  */
+ export function niceToMeetYou(fullName) {
+   const splitArray = fullName.split(/,/);
+   return `Nice to meet you,${splitArray[1]} ${splitArray[0]}`;
+   
+ }
+ 
